82ae2c631a9cee0059a16b5b1698a4fbd326fac4
Todd Laney <ToddLa@Todds-MacBook.local>
2015-06-02 11:18:33 -0700

    Initial Commit

---
+++ GraphCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GraphCalc
+//
+//  Created by Todd Laney on 6/2/15.
+//  Copyright (c) 2015 Todd Laney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
